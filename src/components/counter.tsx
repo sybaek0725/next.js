1c92e105099a4448cb6a1c22962677077ed8977d
soyeon.baek <sybaek0725@gmail.com>
2025-06-11 20:51:30 +0900

    vscode 저장 시 자동 정렬 설정

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,15 +1,15 @@
 'use client'
- 
+
 import { useState } from 'react'
- 
+
 export default function Counter() {
   const [count, setCount] = useState(0)
- 
+
   return (
     <div>
       <p>Count: {count}</p>
-      <button style={{background: '#fff', padding: '0 10px', color: '#000'}} onClick={() => setCount(count + 1)}>Increment</button>
-      <button style={{background: '#000', padding: '0 10px', color: '#fff'}} onClick={() => setCount(count - 1)}>decrement</button>
+      <button style={{ background: '#fff', padding: '0 10px', color: '#000' }} onClick={() => setCount(count + 1)}>Increment</button>
+      <button style={{ background: '#000', padding: '0 10px', color: '#fff' }} onClick={() => setCount(count - 1)}>decrement</button>
     </div>
   )
 }
