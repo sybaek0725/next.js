0bf2237d457c982a076fe2920e328da66883db3a
soyeon.baek <sybaek0725@gmail.com>
2025-07-05 21:26:07 +0900

    postId, postTitle 데이터 바인딩

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,14 +1,36 @@
+'use client'
+
+import { useState } from 'react'
+
 import Counter from '@/components/counter';
 import getPost from '@/app/api/getPost';
 
-export default async function Dashboard() {
-    const post = await getPost('100');
+export default function Dashboard() {
+    const [postId, setPostId] = useState(null)
+    const [postTitle, setPostTitle] = useState(null)
+
+    async function getRandomPost(max: number) {
+        let randomPost = String(Math.floor(Math.random() * max))
+        let data = await getPost(randomPost);
+
+        console.log('getPost : ', data)
+
+        setPostId(data.id)
+        setPostTitle(data.title)
+    }
 
     return (
         <div>
             <h2>Dashboard Page!</h2>
             <Counter />
-            <b>{post.id}. {post.title}</b>
+            <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" onClick={() => getRandomPost(101)}>Random Post Click!</button>
+            {postId && postTitle ?
+                <div>
+                    <div><b>{postId}. {postTitle}</b></div>
+                    <div>{postTitle}</div>
+                </div>
+                : <div><b>There are no posts
+                    Click the Random Post button !</b></div>}
         </div>
 
     )
